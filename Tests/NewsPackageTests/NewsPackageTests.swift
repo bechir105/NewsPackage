6beb7d91da272dca92ebdabbc35eb588e62c737f
bechir105 <81562063+bechir105@users.noreply.github.com>
2024-07-16 10:01:43 +0100

    Update NewsPackageTests.swift

--- Tests/NewsPackageTests/NewsPackageTests.swift
+++ Tests/NewsPackageTests/NewsPackageTests.swift
@@ -2,10 +2,17 @@ import XCTest
 @testable import NewsPackage
 
 final class NewsPackageTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(NewsPackage().text, "Hello, World!")
+    func testFetchTopHeadlines() {
+        let newsManager = NewsManager(apiKey: "9acbb09893ca4c0c95759c1842583feb")
+        
+        let expectation = self.expectation(description: "Fetching top headlines")
+        
+        newsManager.fetchTopHeadlines { articles in
+            XCTAssertNotNil(articles, "Articles should not be nil")
+            XCTAssertGreaterThan(articles?.count ?? 0, 0, "Should fetch at least one article")
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: 10, handler: nil)
     }
 }
